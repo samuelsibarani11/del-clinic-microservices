5c918f85bb9480039912ae94c35cf8bfa278b351
samuelsibarani11 <samuelalbi09@gmail.com>
2024-05-15 23:14:36 +0700

    last-finished

--- Backend/DoctorReport/handler/doctor_report.handler.go
+++ Backend/DoctorReport/handler/doctor_report.handler.go
@@ -4,9 +4,44 @@ import (
 	"doctorReport/database"
 	"doctorReport/model/entity"
 	"doctorReport/utils"
+	"encoding/json"
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	md "medicine/model/entity"
+	"net/http"
+	nr "nurseReport/model/entity"
 )
+
+func GetNurseReportById(nurseReportId int) (*nr.NurseReport, error) {
+	resp, err := http.Get(fmt.Sprintf("http://172.20.10.4:8004/nurse-report/%d", nurseReportId))
+	if err != nil {
+		return nil, fmt.Errorf("failed to make HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var nurseReport nr.NurseReport
+	if err := json.NewDecoder(resp.Body).Decode(&nurseReport); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	return &nurseReport, nil
+}
+
+func GetMedicineById(medicineId int) (*md.Medicine, error) {
+	resp, err := http.Get(fmt.Sprintf("http://172.20.10.4:8004/medicine/%d", medicineId))
+	if err != nil {
+		return nil, fmt.Errorf("failed to make HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var medicine md.Medicine
+	if err := json.NewDecoder(resp.Body).Decode(&medicine); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	return &medicine, nil
+}
 
 func DoctorReportGetAll(ctx *fiber.Ctx) error {
 	var doctorReports []entity.DoctorReport
@@ -66,41 +101,43 @@ func CreateDoctorReport(ctx *fiber.Ctx) error {
 		})
 	}
 
+	reportId := doctorReport.NurseReportID
+	medicineId := doctorReport.MedicineID
+
+	nurseReports, err2 := GetNurseReportById(int(reportId))
+
+	if err2 != nil {
+		return ctx.Status(400).JSON(fiber.Map{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+	}
+
+	medicines, err3 := GetMedicineById(int(medicineId))
+	if err3 != nil {
+		return ctx.Status(400).JSON(fiber.Map{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+	}
+	medicines.ID = uint(medicineId)
+	nurseReports.ID = reportId
+
+	userData := entity.DoctorReport{
+		NurseReportID: nurseReports.ID,
+		Disease:       doctorReport.Disease,
+		Amount:        doctorReport.Amount,
+		StaffDoctorID: doctorReport.StaffDoctorID,
+		MedicineID:    int(medicines.ID),
+	}
+
 	// Create the DoctorReport
-	if err := database.DB.Create(&doctorReport).Error; err != nil {
+	if err := database.DB.Create(&userData).Error; err != nil {
 		return ctx.Status(500).JSON(fiber.Map{
 			"message": "failed to store data",
 			"error":   err.Error(),
 		})
 	}
-
-	// Update Medicine stock
-	//for _, medicine := range doctorReport.Medicines {
-	//	var dbMedicine entity.Medicine
-	//	if err := database.DB.First(&dbMedicine, medicine.ID).Error; err != nil {
-	//		return ctx.Status(400).SendString("Obat tidak ditemukan")
-	//	}
-	//
-	//	if dbMedicine.Amount < medicine.Amount {
-	//		return ctx.Status(400).SendString("Stok obat tidak cukup")
-	//	}
-	//
-	//	dbMedicine.Amount -= medicine.Amount
-	//	if err := database.DB.Save(&dbMedicine).Error; err != nil {
-	//		return ctx.Status(500).JSON(fiber.Map{
-	//			"message": "failed to update medicine stock",
-	//			"error":   err.Error(),
-	//		})
-	//	}
-	//
-	//	// Assign medicine to the current doctor report
-	//	if err := database.DB.Model(&doctorReport).Association("Medicines").Append(&dbMedicine); err != nil {
-	//		return ctx.Status(500).JSON(fiber.Map{
-	//			"message": "failed to associate medicine with doctor report",
-	//			"error":   err.Error(),
-	//		})
-	//	}
-	//}
 
 	// Load relations
 	if err := database.DB.Preload("NurseReport").Preload("StaffDoctor").Preload("Medicines").First(&doctorReport, doctorReport.ID).Error; err != nil {
