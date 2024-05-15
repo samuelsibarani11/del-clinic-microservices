f580cbff40f79a54e2f4cd3fc285c0f2c33e98ac
Your Name <you@example.com>
2024-05-15 11:16:33 +0700

    appointment

--- Backend/Appointment/main.go
+++ Backend/Appointment/main.go
@@ -1,1 +1,25 @@
-package Appointment
+package main
+
+import (
+	"appointment/database"
+	"appointment/database/migration"
+	"appointment/route"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	//INITIAL DATABASE
+	database.DatabaseInit()
+
+	// RUN MIGRATION
+	migration.Migration()
+
+	// menginisialisasikan go fiber (di passing ke route)
+	app := fiber.New()
+
+	// INITIAL ROUTE
+	route.RouteInit(app)
+
+	app.Listen(":8080")
+}
